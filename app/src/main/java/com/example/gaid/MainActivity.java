5c3f87e19c57e56a1336a76cabb11d4efca63f79
heoseungyeon <42290273+heoseungyeon@users.noreply.github.com>
2020-06-29 15:53:58 +0900

    tts update

--- app/src/main/java/com/example/gaid/MainActivity.java
+++ app/src/main/java/com/example/gaid/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.gaid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
 import android.widget.VideoView;
 
-public class MainActivity extends Activity {
+import java.util.Locale;
+
+public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
     private VideoView mVideoview;
+    private TextToSpeech textToSpeech;
+    final int PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,49 @@ public class MainActivity extends Activity {
                 mp.setLooping(true);
             }
         });
+        if ( Build.VERSION.SDK_INT >= 23 ){
+            // 퍼미션 체크
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO},PERMISSION);
+        }
+        init();
 
+        mVideoview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOut("안녕하세요 대양AI센터 입니다 무엇을 도와드릴까요?");
+
+            }
+        });
+    }
+    public void init()
+    {
+        textToSpeech = new TextToSpeech(this, this);
+    }
+    private void speakOut(String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        Log.d("TTS", "SPOKE");
+
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = textToSpeech.setLanguage(Locale.KOREA);
+
+            // tts.setPitch(5); // set pitch level
+
+            // tts.setSpeechRate(2); // set speech speed rate
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            } else {
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
     }
 }
