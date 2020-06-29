4de594ac6ad7ed42e4ea0f4ebd20d3b463857b47
ssook <cesces333@naver.com>
2020-06-29 15:36:13 +0900

    main background video add

--- app/src/main/java/com/example/gaid/MainActivity.java
+++ app/src/main/java/com/example/gaid/MainActivity.java
@@ -3,14 +3,31 @@ package com.example.gaid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
+    private VideoView mVideoview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoview = (VideoView) findViewById(R.id.vv_main);
+        
+        //play video
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.backvideo);
+        mVideoview.setVideoURI(uri);
+        mVideoview.start();
+        //loop
+        mVideoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
 
     }
 }
