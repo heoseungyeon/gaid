39c3e7e2639b7fb135d3eb7b2496e83d9c2b02ab
ssook <cesces333@naver.com>
2020-06-29 15:05:38 +0900

    메인 레이아웃, 시계 추가

--- app/src/main/java/com/example/gaid/MainActivity.java
+++ app/src/main/java/com/example/gaid/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.gaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
