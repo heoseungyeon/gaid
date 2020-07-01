c6810ae73f558a3f4cbde47c05014efacd0a77c8
ssook <cesces333@naver.com>
2020-07-01 18:38:23 +0900

    qr이미지 서버까지 완성

--- app/src/main/java/com/example/gaid/util/RestApi.java
+++ app/src/main/java/com/example/gaid/util/RestApi.java
@@ -11,7 +11,6 @@ import retrofit2.http.Part;
 
 public interface RestApi {
     String BASE_URL = "http://172.16.16.136:8080/";
-
     @Multipart
     @POST("upload") //여기다가 HTTP 통신 방식 넣어야 함.
     Call<SendPictureResponseDTO> send_picture(@Part MultipartBody.Part files);
